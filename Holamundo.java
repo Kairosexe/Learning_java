77b467d6ad165606fdfa4cb57c8cc31da3e13c97
Kairos <gaelm201@gmail.com>
2023-05-15 12:36:53 -0700

    Se agregaron castings de <double> a <float>

--- Holamundo.java
+++ Holamundo.java
@@ -6,18 +6,25 @@ public class Holamundo {
     static float decimal;
     byte es_byte;
 
+    //Este es un ejemplo de una calculadora
     public static void main(String[] args) {
-        double a = 300.165156854116524;
-        double b = 2;
-        float resultado = (float) (a + b);
-        System.out.println("La suma es: " +  resultado);
+        // El tipo de dato double tiene como funcion ejecutar numeros reales dobles.
+        double a = 300.24; 
+        double b = 0.5; 
+        // El tipo de dato float tiene como funcion ejecutar numeros reales simples. 
+        // Este es el cating de "double" a "float".
+        float resultado = (float) (a + b); 
+        System.out.println("La suma es: " + resultado);
 
-        resultado  = (float) (a /  b);
+        // Este es el cating de "double" a "float".
+        resultado  = (float) (a / b);
         System.out.println("La division es: " + resultado);
         
+        // Este es el cating de "double" a "float". 
         resultado  = (float) (a - b);
         System.out.println("La resta es: " + resultado);
         
+        // Este es el cating de "double" a "float".
         resultado  = (float) (a * b);
         System.out.println("La multiplicacion es: " + resultado);
     }
