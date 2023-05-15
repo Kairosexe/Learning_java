a8a48b18bc256f26f3e3d4fd0cdefe53e84165e6
Kairos <gaelm201@gmail.com>
2023-05-15 10:27:17 -0700

    fix: Marca error dentro de los resutados de <a + b>

--- Holamundo.java
+++ Holamundo.java
@@ -1,5 +1,24 @@
 public class Holamundo {
+
+    char A = 'A';
+    int a = 789 ,b = 465 ,c = 123;
+    boolean binario = true;
+    static float decimal;
+    byte es_byte;
+
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        double a = 300.165156854116524;
+        double b = 2;
+        float resultado = (float) (a + b);
+        System.out.println("La suma es: " +  resultado);
+
+        resultado  = (float) (a /  b);
+        System.out.println("La division es: " + resultado);
+        
+        resultado  = (float) (a - b);
+        System.out.println("La resta es: " + resultado);
+        
+        resultado  = (float) (a * b);
+        System.out.println("La multiplicacion es: " + resultado);
     }
 }
