596d8a076abddc625341c52ff1cefc3050e97713
Andres-GMP <al20760260@ite.edu.mx>
2023-05-17 04:21:05 -0700

    feat: new assignment
    Solve the next problem and show me your skills

---
+++ Conditionals.java
@@ -0,0 +1,40 @@
+public class Conditionals {
+    // Para comprender este concepto recomiendo explorar el siguiente enlace que ya
+    // nos brinda nuestro roadmap: https://www.javatpoint.com/java-if-else
+    // La idea principal está en que explores las condiciones: if, if else, if else
+    // if... y condiciones anidadas (nestest if)
+    // Enseguido tendrás un ejemplo de una condición:
+
+    public static void main(String[] args) {
+        int tiempo_andres = 0;
+        if (tiempo_andres > 10) {
+            System.out.println("Andrés tiene tiempo de enseñarte un poco el día de hoy... ☜(ﾟヮﾟ☜)");
+        } else {
+            System.out.println("Andrés NO tiene tiempo de enseñarte un poco el día de hoy... ¯-_(ツ)_/¯");
+        }
+    }
+    // ¿ENTONCES AHORA QUE SIGUE?
+    // Temas a explorar antes de comenzar hacer código: condiciones (estructuras de
+    // control -> Estructuras condicional), operadores, diagramas de flujo
+
+    // Como programador es muy importante saber aplicar estas condiciones en
+    // problemas "cotidianos" recuerda que ya no eres una persona común y corriente,
+    // tus problemas
+    // ahora se tornaron respecto a datos, variables y tu habilidad de darle
+    // instrucciones a una computadora; ahora eres un desarrollador Java en una
+    // empresa y manejan tipos
+    // de datos númericos para sus registros, así que
+    // comencemos por ahí:
+
+    // TODO: CREA UN PROGRAMA CAPAZ DE COMPROBAR SI UN NÚMERO ES POSITIVO, NEGATIVO,
+    // CERO, PAR, IMPAR, DIVISIBLE ENTRE OTRO NÚMERO.
+
+    // NOTE: En el tema Estructuras de control no es necesario que explroes todas
+    // las disponibles en Java, basta con las de control basicas
+    // NOTE: En el programa no es necesario lectura de datos aún, por ahora serán
+    // variables declaradas.
+
+    // En caso de no tener donde desarrollar tu código (laptop o pc) sugiero
+    // descargar Online Compiler - Code en Mobi
+    // Es una aplicación de celular
+}
