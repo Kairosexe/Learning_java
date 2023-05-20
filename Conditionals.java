94a755a12a00b545f07dca9ba55ebce5575edd99
Kairos <gaelm201@gmail.com>
2023-05-19 18:27:00 -0700

    Estoy creando un programa capaz de comparbar si un numero es positivo, negativo, cero, par, impar y divisible entre otro numero.

---
+++ Conditionals.java
@@ -0,0 +1,44 @@
+public class Conditionals {
+    public static void main(String[] args) {
+
+        int numero = 5;
+
+        // Confrima si el numero es positivo o negativo.
+        if (numero > 0) {
+            System.out.println("Es positivo.");
+        } else {
+            System.out.println("Es negativo.");
+        }
+        
+        // Confrima si el numero es "Cero".
+        if (numero == 0) {
+            System.out.println("Es cero");
+        } else {
+            System.out.println("La neta no se que es esto... *explota*.");
+        }
+
+        // Confrima si el numero es par o inpar.
+        if (numero % 2 == 0) {
+            System.out.println("Es par.");
+        } else {
+            System.out.println("Es impar.");
+        } 
+        
+        // Confrima si el numero es divisible entre otro numero.
+        if (numero ) {
+            System.out.println("");
+        } else {
+            System.out.println("");
+        } 
+        
+    }
+}
+
+// public static void main(String[] args) {
+//     int tiempo_andres = 10;
+//     if (tiempo_andres > 10) {
+//         System.out.println("Andrés tiene tiempo de enseñarte un poco el día de hoy... ☜(ﾟヮﾟ☜)");
+//     } else {
+//         System.out.println("Andrés NO tiene tiempo de enseñarte un poco el día de hoy... ¯-_(ツ)_/¯");
+//     }
+// }
