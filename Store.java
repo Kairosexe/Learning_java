cc209795f9107aac17fa4dc7ec397d4e2cdca2a2
Kairos <gaelm201@gmail.com>
2023-05-28 17:53:41 -0700

    feat: Se crearon las declaraciones con valores asignados.

--- Store.java
+++ Store.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class Store {
 
     public static void main(String[] args) {
         int opcion = 0;
+        int pollo1Kilo = 24; 
+        int papa1Kilo = 17; 
+        int queso1Unidad = 157; 
+        int tosino1Kilo = 92;
+        int cantidad;
 
         // ESTRUCTURA DE CONTROL ITERATIVA VARIABLE: No es necesario que la entiendas
         // ahorita
@@ -9,6 +16,7 @@ public class Store {
 
         while (opcion == 5) {
             // TODO: AQUI DENTRO VA EL CÓDIGO.
+
         }
     }
 }
