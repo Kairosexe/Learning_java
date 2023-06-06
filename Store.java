09dc3fb205ec40077738475fc62d1b3d41e542dd
Kairos <gaelm201@gmail.com>
2023-06-06 15:49:15 -0700

    fix: Se soliciono la alerta en el nombre que recibe <scanner>agregando <scan.close() en la linea 97 del codigo fuera del bloque de <while>.

--- Store.java
+++ Store.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Store {
     public static void main(String[] args) {
-        
+
         int opcion = 1;
 
         // Datos de los productos de la tienda.
@@ -11,27 +11,32 @@ public class Store {
         int queso1Unidad = 157;
         int tosino1Kilo = 92;
 
-        // Varibles a las cuales se les asignara un valor por medio de la clase "Scanner".
+        // Varibles a las cuales se les asignara un valor por medio de la clase
+        // "Scanner".
         int numProducto;
         int cantidad;
 
-        // Clase para capturar datos. 
-        // Presenta un error, pero al parecer el codigo funciona.
+        // Clase para capturar datos.
         Scanner scan = new Scanner(System.in);
 
-        // Ejecuta lo que este dentro de su bloque en modo de bucle simepre y cuando la condicion sea verdadera, en este caso, si la variable "opcion" se le da el valor de "1" el bucle continuara, pero si el valor de esta cambia a "0", el bucle se cerrara.
+        // Ejecuta lo que este dentro de su bloque en modo de bucle simepre y cuando la
+        // condicion sea verdadera, en este caso, si la variable "opcion" se le da el
+        // valor de "1" el bucle continuara, pero si el valor de esta cambia a "0", el
+        // bucle se cerrara.
         // EJEMPLO:
         // Si "opcion" es diferente a 0 el bucle continua.
         while (opcion != 0) {
 
             System.out.println("\tBIENVENIDO/A \nNUESTROS PRODUCTOS SON:");
-            System.out.println("1. Pollo $24 el Kilo. \n2. Papa $17 el Kilo. \n3. Queso $157 la unidad. \n4. Tosino $92 el Kilo.");
+            System.out.println(
+                    "1. Pollo $24 el Kilo. \n2. Papa $17 el Kilo. \n3. Queso $157 la unidad. \n4. Tosino $92 el Kilo.");
 
             System.out.println("Ingresa el numero del producto que llevaras.");
             // Forma parte de la calse "Scanner", aqui se asignan valores.
             numProducto = scan.nextInt();
 
-            // Si selecciona algun numero del 1-4 retornara en consola la opcion de acuerdo el caso que le fue asignado. 
+            // Si selecciona algun numero del 1-4 retornara en consola la opcion de acuerdo
+            // el caso que le fue asignado.
             switch (numProducto) {
                 case 1:
                     System.out.println("Selecciono Pollo.");
@@ -48,16 +53,19 @@ public class Store {
             }
 
             System.out.println("Ingresa la cantidad que llevaras.");
-            // Aqui se asigna valor a la variable "cantidad" que representara la cantidad del producto que se selecciono.
+            // Aqui se asigna valor a la variable "cantidad" que representara la cantidad
+            // del producto que se selecciono.
             cantidad = scan.nextInt();
             System.out.println("CARRITO DE COMPRAS:");
-            
-            // Si la cantidad del producto es mayor a cero retornara la cantidad que se le asigno en la terminal.
+
+            // Si la cantidad del producto es mayor a cero retornara la cantidad que se le
+            // asigno en la terminal.
             if (cantidad > 0) {
                 System.out.println("Cantidad: " + cantidad + "Kg.");
             }
 
-            // De acuerdo a el caso se selecciono se retornara el precio del producto multiplicado por la cantidad que el cliente solicito.
+            // De acuerdo a el caso se selecciono se retornara el precio del producto
+            // multiplicado por la cantidad que el cliente solicito.
             switch (numProducto) {
                 case 1:
                     System.out.println("Precio: $" + pollo1Kilo * cantidad);
@@ -75,12 +83,17 @@ public class Store {
 
             System.out.println("¿Desea salir o continuar?");
             System.out.println("Preciona <1> para continuar o <0> para salir.");
-            // Aqui se le asignara el valor de acurdo a lo que el cliente desee hacer segun las indicaciones que se le dio.
+            // Aqui se le asignara el valor de acurdo a lo que el cliente desee hacer segun
+            // las indicaciones que se le dio.
             opcion = scan.nextInt();
-            // Si "opcion" es igual a "0" retornara "¡Gracias, vuelva pronto!" y el programa finalizara.
+            // Si "opcion" es igual a "0" retornara "¡Gracias, vuelva pronto!" y el programa
+            // finalizara.
             if (opcion == 0) {
                 System.out.println("¡Gracias, vuelva pronto!");
             }
         }
+        // Cierra la secuencia y libera los recursos que estaban ocupados en la secuencia de scanner.
+        // Basicamente esta clase libera espacio en la memoria. 
+        scan.close();
     }
 }
