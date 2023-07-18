f953e46c3171e5f14cb5f352a36a5d8d93a3d21e
Andres-GMP <al20760260@ite.edu.mx>
2023-07-18 11:06:07 -0700

    feat: its just new code

---
+++ Tienda.java
@@ -0,0 +1,15 @@
+public class Tienda {
+    public static void main(String[] args) {
+        int opcion = 0;
+        int accion = 0;
+        double pollo[] = { 1, 24, 100 };
+        System.out.println("Ingrese la opcion a realizar \n1) Compra \n2) Venta\n3)Ver productos \n4)Salir");
+
+        while (opcion != 4) {
+            switch (accion) {
+                case 1:
+                    System.out.println();
+            }
+        }
+    }
+}
