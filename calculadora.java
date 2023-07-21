4215df5036927d82a2960189536418e27da546a1
Kairos <gaelm201@gmail.com>
2023-07-20 18:20:05 -0700

    feat: Se modifico la calculadora agregadole difernetes tipos de operaciones.

--- calculadora.java
+++ calculadora.java
@@ -1,17 +1,59 @@
 import java.util.Scanner;
 
 public class calculadora {
+    static Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        
+        int opcion = 0;
 
-        int multiplo = 5;
-        int numero = 0;
+        float numero1 = 0;
+        float numero2 = 0;
+        float total = 0;
 
-        while (numero != 10) {
-            System.out.println("Ingresa un numero:");
-            numero = scan.nextInt();
-            System.out.println(multiplo * numero);
-
+        while (opcion != 5) {
+            System.out.println("---------------");
+            System.out.println("CALCULADORA");
+            System.out.println("---------------");
+            System.out.println("1. Sumar. \n2. Restar. \n3. Multplicar. \n4. Dividir \n5. Salir. ");
+            System.out.println("\nSelecciona una opcion.");
+            opcion = scan.nextInt();
+            switch (opcion) {
+                case 1:
+                    System.out.println("Ingresa el primer numero:");
+                    numero1 = scan.nextFloat();
+                    System.out.println("Ingresa el segundo numero:");
+                    numero2 = scan.nextFloat();
+                    total = numero1 + numero2;
+                    System.out.println("Resultado = " + total);
+                    break;
+                case 2:
+                    System.out.println("Ingresa el primer numero:");
+                    numero1 = scan.nextFloat();
+                    System.out.println("Ingresa el segundo numero:");
+                    numero2 = scan.nextFloat();
+                    total = numero1 - numero2;
+                    System.out.println("Resultado = " + total);
+                    break;
+                case 3:
+                    System.out.println("Ingresa el primer numero:");
+                    numero1 = scan.nextFloat();
+                    System.out.println("Ingresa el segundo numero:");
+                    numero2 = scan.nextFloat();
+                    total = numero1 * numero2;
+                    System.out.println("Resultado = " + total);
+                    break;
+                case 4:
+                    System.out.println("Ingresa el primer numero:");
+                    numero1 = scan.nextFloat();
+                    System.out.println("Ingresa el segundo numero:");
+                    numero2 = scan.nextFloat();
+                    total = numero1 / numero2;
+                    System.out.println("Resultado = " + total);
+                    break;
+                case 5:
+                    System.out.println("Nos vemos pronto.");
+                    break;
+            }
         }
         scan.close();
     }
